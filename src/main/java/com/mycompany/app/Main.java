905b96ce65a946dfae56785a550a26912b70e666
Mattias Andersson <mattias.j.andersson@gmail.com>
2022-12-20 20:30:55 +0100

    gradle

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,7 +1,13 @@
 package com.mycompany.app;
 
 public class Main {
-	public static void main(String args[]) {
-		System.out.println(Calculator.sum(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
-	}
+    public static void main(String args[]) {
+        if(args.length == 2) {
+            var res = Calculator.sum(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            System.out.println(res);
+        }
+        else {
+            System.err.println("usage: n n");
+        }
+    }
 }
--- src/test/java/com/mycompany/app/CalculatorTest.java
+++ src/test/java/com/mycompany/app/CalculatorTest.java
@@ -7,16 +7,15 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-	@DisplayName("should sum positive numbers")
-	@Test
-	public void shouldSumPositiveNumbers() {
-		assertThat(Calculator.sum(2, 3)).isEqualTo(5);
-	}
+    @DisplayName("should sum positive numbers")
+    @Test
+    public void shouldSumPositiveNumbers() {
+        assertThat(Calculator.sum(2, 3)).isEqualTo(5);
+    }
 
-	@DisplayName("should sum positive and negative numbers")
-	@Test
-	public void shouldSumPositiveAndNegativeNumbers() {
-		assertThat(Calculator.sum(2, -3)).isEqualTo(-1);
-	}
+    @DisplayName("should sum positive and negative numbers")
+    @Test
+    public void shouldSumPositiveAndNegativeNumbers() {
+        assertThat(Calculator.sum(2, -3)).isEqualTo(-1);
+    }
 }
-
